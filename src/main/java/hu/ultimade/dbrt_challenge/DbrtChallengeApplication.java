7e98ae342c1774135bdc5ca57605a7f11252c4c6
W.Zsolt <wzsolt1990@gmail.com>
2025-05-23 11:42:51 +0200

    Basic resolve of the tasks. missing classes

---
+++ src/main/java/hu/ultimade/dbrt_challenge/DbrtChallengeApplication.java
@@ -0,0 +1,16 @@
+package hu.ultimade.dbrt_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@SpringBootApplication
+public class DbrtChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbrtChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/ultimade/dbrt_challenge/DbrtChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.ultimade.dbrt_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbrtChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
